70e04dbe5fe58f88ca71c7a06b339bfecd493f5a
Evelyn House <eve@ehouse.io>
2022-01-23 12:51:23 -0500

    Removing broken local auth code

--- pages/notes/[id].tsx
+++ pages/notes/[id].tsx
@@ -1,41 +1,87 @@
-import { withSSRContext } from 'aws-amplify';
-import { Note } from '../../src/models';
-import Markdown from 'react-markdown';
-import { useRouter } from 'next/router';
+import { Amplify, API, withSSRContext } from "aws-amplify";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
-export default function PostComponent({ note }) {
-    const router = useRouter();
-    if (router.isFallback) {
-        return <div>Loading...</div>;
-    }
-    return (
-        <div>
-            <Markdown children={note.body} />
-        </div>
-    );
-}
+import awsExports from "../../src/aws-exports";
+import { deleteNote } from "../../src/graphql/mutations";
+import { getNote, listNotes } from "../../src/graphql/queries";
 
-export async function getStaticPaths(req) {
-    const { DataStore } = withSSRContext(req);
-    const notes = await DataStore.query(Note);
-    const paths = notes.map(note => ({ params: { id: note.id } }));
+import styles from "../../styles/Home.module.css";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+export async function getStaticPaths() {
+    const SSR = withSSRContext();
+    const { data } = await SSR.API.graphql({ query: listNotes });
+    const paths = data.listNotes.items.map((note) => ({
+        params: { id: note.id },
+    }));
+
     return {
-        paths,
         fallback: true,
+        paths,
     };
 }
 
-export async function getStaticProps(req) {
-    const { DataStore } = withSSRContext(req);
-    const { params } = req;
-    const { id } = params;
-    const note = await DataStore.query(Note, id);
+export async function getStaticProps({ params }) {
+    const SSR = withSSRContext();
+    const { data } = await SSR.API.graphql({
+        query: getNote,
+        variables: {
+            id: params.id,
+        },
+    });
 
     return {
         props: {
-            note: JSON.parse(JSON.stringify(note))
+            note: data.getNote,
         },
-        revalidate: 1
     };
+}
 
+export default function Note({ note }) {
+    const router = useRouter();
+
+    if (router.isFallback) {
+        return (
+            <div className={styles.container}>
+                <h1 className={styles.title}>Loading&hellip;</h1>
+            </div>
+        );
+    }
+
+    async function handleDelete() {
+        try {
+            await API.graphql({
+                authMode: "AMAZON_COGNITO_USER_POOLS",
+                query: deleteNote,
+                variables: {
+                    input: { id: note.id },
+                },
+            });
+
+            window.location.href = "/";
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>{note.title} – Amplify + Next.js</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>{note.title}</h1>
+
+                <p className={styles.description}>{note.body}</p>
+            </main>
+
+            <footer className={styles.footer}>
+                <button onClick={handleDelete}>💥 Delete note</button>
+            </footer>
+        </div>
+    );
 }
