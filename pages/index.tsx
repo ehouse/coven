55137a15dd74f940eae4207828432202991b5a38
Evelyn House <eve@ehouse.io>
2022-02-02 19:12:32 -0500

    Created responsive front-page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import '@aws-amplify/ui-react/styles.css';
-import { AppShell, Group, Header, Button, Grid, Title, Text, Center, Container } from '@mantine/core';
-import { Card, List, Divider, ThemeIcon, useMantineTheme } from '@mantine/core';
+import { Affix, AppShell, Group, Header, Button, Grid, Title, Text, Center, Container, Navbar, Burger } from '@mantine/core';
+import { Card, List, MediaQuery, ThemeIcon, useMantineTheme } from '@mantine/core';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import { RiBook2Fill, RiShieldKeyholeFill, RiMindMap } from "react-icons/ri";
 
 const Home: NextPage = () => {
+    const [expanded, setExpanded] = useState(false);
     const theme = useMantineTheme();
 
     const secondaryColor = theme.colorScheme === 'dark'
@@ -16,32 +17,72 @@ const Home: NextPage = () => {
     const router = useRouter();
 
     return (<AppShell
-        header={
-            <Header
-                height={80}
-                padding={'lg'}
-                sx={{
-                    boxShadow: theme.shadows.md
-                }}
+        navbar={
+            <MediaQuery
+                query="(min-width: 800px)"
+                styles={{ display: 'none' }}
             >
-                <Grid>
-                    <Grid.Col span={6}>
-                        <Title order={2}>SpiderNotes</Title>
-                    </Grid.Col>
-                    <Grid.Col span={6}>
-                        <Group position='right' direction='row'>
-                            <Button component="a" href="/notebook" size='md' variant="white" color="red">
-                                Log In
-                            </Button>
-                            <Button component="a" href="/notebook" variant="gradient" gradient={{ from: 'pink', to: 'red', deg: 35 }}>
-                                New Account
-                            </Button>
-                        </Group>
-                    </Grid.Col>
-                </Grid>
-            </Header>
+                <Navbar
+                    padding="md"
+                    // Breakpoint at which navbar will be hidden if hidden prop is true
+                    hiddenBreakpoint="lg"
+                    // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
+                    hidden={!expanded}
+                    // when viewport size is less than theme.breakpoints.sm navbar width is 100%
+                    // viewport size > theme.breakpoints.sm – width is 300px
+                    // viewport size > theme.breakpoints.lg – width is 400px
+                    width={{ sm: 300, md: 300 }}
+                >
+                    <Text>Navigation</Text>
+                    <Button component="a" href="/notebook" size='md' variant="white" color="red">
+                        Log In
+                    </Button>
+                    <Button component="a" href="/notebook" variant="gradient" gradient={{ from: 'pink', to: 'red', deg: 35 }}>
+                        New Account
+                    </Button>
+                </Navbar>
+            </MediaQuery>
+        }
+        header={
+            <MediaQuery
+                query="(max-width: 800px)"
+                styles={{ display: 'none' }}
+            >
+                <Header
+                    height={80}
+                    padding={'lg'}
+                    sx={{
+                        boxShadow: theme.shadows.md
+                    }}
+                >
+                    <Grid>
+                        <Grid.Col span={6}>
+                            <Title order={2}>SpiderNotes</Title>
+                        </Grid.Col>
+                        <Grid.Col span={6}>
+                            <Group position='right' direction='row'>
+                                <Button component="a" href="/notebook" size='md' variant="white" color="red">
+                                    Log In
+                                </Button>
+                                <Button component="a" href="/notebook" variant="gradient" gradient={{ from: 'pink', to: 'red', deg: 35 }}>
+                                    New Account
+                                </Button>
+                            </Group>
+                        </Grid.Col>
+                    </Grid>
+                </Header>
+            </MediaQuery>
         }>
         <Container size={'xl'}>
+            <MediaQuery largerThan={800} styles={{ display: 'none' }}>
+                <Burger
+                    opened={expanded}
+                    onClick={() => setExpanded((o) => !o)}
+                    size="lg"
+                    color={theme.colors.gray[6]}
+                    mr="xl"
+                />
+            </MediaQuery>
             <div style={{ padding: '4rem 0 7rem 0' }}>
                 <Center mb={'xl'}>
                     <Title sx={{ fontSize: 68, fontWeight: '700' }}>Spider Notes</Title>
@@ -52,7 +93,7 @@ const Home: NextPage = () => {
             </div>
             <Grid gutter="xl" >
                 <Grid.Col md={12} lg={4}>
-                    <Card shadow="md" padding="lg" sx={{ height: '15rem' }}>
+                    <Card shadow="md" padding="lg" sx={{ minHeight: '16rem' }}>
                         <Card.Section sx={{ boxShadow: theme.shadows.sm }}>
                             <div style={{
                                 width: 'auto',
@@ -70,13 +111,13 @@ const Home: NextPage = () => {
                         <Text size="md" style={{ marginBottom: theme.spacing.sm, marginTop: theme.spacing.sm }}>
                             <List spacing={'sm'}>
                                 <List.Item>Designed from the ground up to minimize distractions, keep writing!</List.Item>
-                                <List.Item>Minimize the clutter to reduce information overload by hiding unneccicary notes.</List.Item>
+                                <List.Item>Minimize the clutter to reduce information overload by hiding unnecessary notes.</List.Item>
                             </List>
                         </Text>
                     </Card>
                 </Grid.Col>
                 <Grid.Col md={12} lg={4}>
-                    <Card shadow="md" padding="lg" sx={{ height: '15rem' }}>
+                    <Card shadow="md" padding="lg" sx={{ minHeight: '16rem' }}>
                         <Card.Section sx={{ boxShadow: theme.shadows.sm }}>
                             <div style={{
                                 width: 'auto',
@@ -93,14 +134,14 @@ const Home: NextPage = () => {
                         </Card.Section>
                         <Text size="md" style={{ marginBottom: theme.spacing.sm, marginTop: theme.spacing.sm }}>
                             <List spacing={'sm'}>
-                                <List.Item>Connect relevent topics together to create a map of knowedge.</List.Item>
+                                <List.Item>Connect relevent topics together to create a map of knowledge.</List.Item>
                                 <List.Item>Powerful search feature to find exactly what you are looking for.</List.Item>
                             </List>
                         </Text>
                     </Card>
                 </Grid.Col>
                 <Grid.Col md={12} lg={4}>
-                    <Card shadow="md" padding="lg" sx={{ height: '15rem' }}>
+                    <Card shadow="md" padding="lg" sx={{ minHeight: '16rem' }}>
                         <Card.Section sx={{ boxShadow: theme.shadows.sm }}>
                             <div style={{
                                 width: 'auto',
@@ -118,9 +159,9 @@ const Home: NextPage = () => {
 
                         <Text size="md" style={{ marginBottom: theme.spacing.sm, marginTop: theme.spacing.sm }}>
                             <List spacing={'sm'}>
-                                <List.Item>All information is stored encrypted in the cloud. </List.Item>
+                                <List.Item>All information is stored encrypted in the cloud.</List.Item>
                                 <List.Item>Never again worry about losing data or forgetting which notebook you wrote something in.</List.Item>
-                                <List.Item>Download data backups for peace of mind</List.Item>
+                                <List.Item>Download data backups for peace of mind.</List.Item>
                             </List>
                         </Text>
                     </Card>
